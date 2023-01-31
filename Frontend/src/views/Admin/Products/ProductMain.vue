50e16ab51ee0d5115704fba24a3bba9ddfe99ac3
MinhLuongVan <luongvanminh290@gmail.com>
2023-01-31 23:27:49 +0700

    update delete-edit product

--- Frontend/src/views/Admin/Products/ProductMain.vue
+++ Frontend/src/views/Admin/Products/ProductMain.vue
@@ -10,7 +10,7 @@
       </button>
     </div>
     <div class="block lg:flex justify-between">
-      <span class="lg:pt-2.5 text-slate-700">Tổng số sản phẩm : 5</span>
+      <span class="lg:pt-2.5 text-slate-700">Tổng số sản phẩm : {{ products.length }}</span>
       <div class="flex">
         <div class="min-w-[215px] max-w-sm relative my-2 lg:my-0">
           <input
@@ -40,7 +40,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(item, index) in products" :key="index" class="intro-x">
+            <tr v-for="item in products" :key="item._id" class="intro-x">
               <td class="w-40">
                 <div class="w-10 h-10 image-fit zoom-in">
                   <img
@@ -57,13 +57,17 @@
               <td class="text-center pt-4">{{ item.category }}</td>
               <td class="table-report__action w-56">
                 <div class="flex justify-center items-center pt-4">
-                  <a class="flex items-center mr-3" href="javascript:;">
+                  <a
+                    class="flex items-center mr-3"
+                    href="javascript:;"
+                    @click="actionInitEditProduct(item)"
+                  >
                     <CheckSquareIcon class="w-4 h-4 mr-1" /> Chỉnh sửa
                   </a>
                   <a
                     class="flex items-center text-danger"
                     href="javascript:;"
-                    @click="deleteConfirmationModal = true"
+                    @click="actionInitDeleteProduct(item)"
                   >
                     <Trash2Icon class="w-4 h-4 mr-1" /> Xóa
                   </a>
@@ -72,35 +76,41 @@
             </tr>
           </tbody>
         </table>
+        <!-- BEGIN: Delete Confirmation Modal -->
+        <Modal
+          :show="deleteConfirmationModal"
+          @hidden="deleteConfirmationModal = false"
+        >
+          <ModalBody class="p-0">
+            <div class="p-5 text-center">
+              <XCircleIcon class="w-16 h-16 text-danger mx-auto mt-3" />
+              <div class="text-3xl mt-5">Xóa sản phẩm</div>
+              <div class="text-slate-500 mt-2">
+                Bạn có chắc chắn muốn xóa sản phẩm này?
+              </div>
+            </div>
+            <div class="px-5 pb-8 text-center">
+              <button
+                type="button"
+                @click="deleteConfirmationModal = false"
+                class="btn btn-outline-secondary w-24 mr-1"
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger w-24"
+                @click="actionDeleteProduct"
+              >
+                Delete
+              </button>
+            </div>
+          </ModalBody>
+        </Modal>
+        <!-- END: Delete Confirmation Modal -->
       </div>
       <!-- END: Data List -->
-      <!-- BEGIN: Delete Confirmation Modal -->
-      <Modal
-        :show="deleteConfirmationModal"
-        @hidden="deleteConfirmationModal = false"
-      >
-        <ModalBody class="p-0">
-          <div class="p-5 text-center">
-            <XCircleIcon class="w-16 h-16 text-danger mx-auto mt-3" />
-            <div class="text-3xl mt-5">Xóa sản phẩm</div>
-            <div class="text-slate-500 mt-2">
-              Bạn có chắc chắn muốn xóa sản phẩm này?
-            </div>
-          </div>
-          <div class="px-5 pb-8 text-center">
-            <button
-              type="button"
-              @click="deleteConfirmationModal = false"
-              class="btn btn-outline-secondary w-24 mr-1"
-            >
-              Cancel
-            </button>
-            <button type="button" class="btn btn-danger w-24">Delete</button>
-          </div>
-        </ModalBody>
-      </Modal>
-      <!-- END: Delete Confirmation Modal -->
-  </div>
+    </div>
     <!-- BEGIN: Add,Edit Confirmation Modal -->
     <Modal :show="AddConfirmationModal" @hidden="AddConfirmationModal = false">
       <ModalBody class="p-0">
@@ -119,7 +129,12 @@
           </div>
           <div class="px-5">
             <label class="text-base">Số lượng</label>
-            <input type="number" class="form-control my-2" min="0" v-model="amount"/>
+            <input
+              type="number"
+              class="form-control my-2"
+              min="0"
+              v-model="amount"
+            />
           </div>
           <div class="px-5">
             <label class="text-base">Giá</label>
@@ -157,17 +172,17 @@
             <label class="text-base">Hình ảnh</label>
             <!-- BEGIN: Single File Upload -->
             <input
-                type="file"
-                @change="uploadAvatar"
-                class="intro-x login__input form-control py-3 px-4 block mt-1"
-              />
+              type="file"
+              @change="uploadAvatar"
+              class="intro-x login__input form-control py-3 px-4 block mt-1"
+            />
             <!-- END: Single File Upload -->
             <div
-                class="text-danger mt-3"
-                v-if="chosenFile && !chosenFile.type.includes('image/')"
-              >
-                Đây không phải là file hình ảnh
-              </div>
+              class="text-danger mt-3"
+              v-if="chosenFile && !chosenFile.type.includes('image/')"
+            >
+              Đây không phải là file hình ảnh
+            </div>
           </div>
         </div>
         <div class="pb-5 pt-2 text-center lg:pl-56 lg:pr-3">
@@ -179,11 +194,20 @@
             Trở lại
           </button>
           <button
+            v-if="!showButtonEdit"
             type="button"
             class="btn text-white bg-lime-500"
             @click="actionSaveProduct"
           >
             Lưu và đóng
+          </button>
+          <button
+            v-else
+            type="button"
+            class="btn text-white bg-lime-500"
+            @click="actionEditProduct"
+          >
+            Cập nhật
           </button>
         </div>
       </ModalBody>
@@ -192,20 +216,24 @@
   </div>
 </template>
 <script lang="ts">
-import {  defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import { useAuthStore } from "../../../stores/authStore";
-import { productInfor } from '../../../types/productType';
+import { productInfor } from "../../../types/productType";
 import productService from "../../../services/productService";
 import { setNotificationToastMessage } from "../../../utils/myFunction";
-import {useRouter} from "vue-router";
+import { useRouter } from "vue-router";
+import { ProductModel } from "../../../model/productModel";
 export default defineComponent({
   name: "Products",
   setup() {
     const router = useRouter();
     const deleteConfirmationModal = ref(false);
     const AddConfirmationModal = ref(false);
+    const showButtonEdit = ref(false);
     const authStore = useAuthStore();
+    const idUpdate = ref("");
     const products = ref<productInfor[]>([]);
+    const selectedProduct = ref<ProductModel>(new ProductModel());
     const name = ref("");
     const image = ref("");
     const price = ref(0);
@@ -215,42 +243,42 @@ export default defineComponent({
     const chosenFile: any = ref(null);
     const Category = [
       {
-        name: "Cây phong thủy"
+        name: "Cây phong thủy",
       },
       {
-        name: "Cây treo"
+        name: "Cây treo",
       },
       {
-        name: "Cây dàn leo"
+        name: "Cây dàn leo",
       },
       {
-        name: "Cây sen đá"
+        name: "Cây sen đá",
       },
       {
-        name: "Cây xương rồng"
+        name: "Cây xương rồng",
       },
       {
-        name: "Chậu cảnh"
+        name: "Chậu cảnh",
       },
-    ]
-  
-    async function uploadAvatar(event:any) {
+    ];
+
+    async function uploadAvatar(event: any) {
       chosenFile.value = event.target.files[0];
     }
     // Get all product
     async function initGetAllProduct() {
-        const data = {} as productInfor;
-        const response = await productService.findAll(data,authStore.token);
-        // products.value = response.data.values;
-        if (response.data.success) {
-          products.value = response.data.values;
-          } else {
-            setNotificationToastMessage("Tải dữ liệu thât bại", false)
-          }
+      const data = {} as productInfor;
+      const response = await productService.findAll(data, authStore.token);
+      // products.value = response.data.values;
+      if (response.data.success) {
+        products.value = response.data.values;
+      } else {
+        setNotificationToastMessage("Tải dữ liệu thât bại", false);
+      }
     }
     onMounted(() => {
       initGetAllProduct();
-    })
+    });
     // Save product
     const actionSaveProduct = async () => {
       const data = {
@@ -259,25 +287,76 @@ export default defineComponent({
         price: price.value,
         discount: discount.value,
         category: category.value,
-        image: chosenFile.value  
+        image: chosenFile.value,
       } as productInfor;
-      console.log('test',data)
-      const response = await productService.save(data,authStore.token);
+      const response = await productService.save(data, authStore.token);
       if (response.data.success) {
         AddConfirmationModal.value = false;
         initGetAllProduct();
       } else {
         setNotificationToastMessage("Tải dữ liệu thất bại", false);
       }
+    };
+
+    function actionInitDeleteProduct(item: ProductModel) {
+      selectedProduct.value._id = item._id;
+      deleteConfirmationModal.value = true;
+    }
+
+    // Delete product
+    async function actionDeleteProduct() {
+      const itemDelete = new ProductModel();
+      itemDelete._id = selectedProduct.value._id;
+      const response = await productService.delete(itemDelete, authStore.token);
+      if (response.data.error) {
+        setNotificationToastMessage("Xóa dữ liệu thất bại", false);
+      } else {
+        deleteConfirmationModal.value = false;
+        initGetAllProduct();
+      }
+    }
+
+    async function actionInitEditProduct(item: productInfor) {
+      const itemFindId = { _id: item._id } as productInfor;
+      const response = await productService.findOne(
+        itemFindId,
+        authStore.token
+      );
+      idUpdate.value = response.data.values._id;
+      name.value = response.data.values.name;
+      amount.value = response.data.values.amount;
+      discount.value = response.data.values.discount;
+      price.value = response.data.values.price;
+      image.value = response.data.values.image;
+      category.value = response.data.values.category;
+      AddConfirmationModal.value = true;
+      showButtonEdit.value = true;
+    }
+
+    async function actionEditProduct() {
+      const dataUpdate = {
+        _id: idUpdate.value,
+        name: name.value,
+        amount: amount.value,
+        price: price.value,
+        discount: discount.value,
+        category: category.value,
+        image: chosenFile.value,
+      } as productInfor;
+      const response = await productService.update(dataUpdate, authStore.token);
+      if (response.data.success) {
+        AddConfirmationModal.value = false;
+        initGetAllProduct();
+      } else {
+        setNotificationToastMessage("Cập nhật dữ liệu thất bại", false);
+      }
     }
 
     return {
       router,
-      AddConfirmationModal,
-      deleteConfirmationModal,
-      actionSaveProduct,
       Category,
       image,
+      idUpdate,
       name,
       discount,
       price,
@@ -285,7 +364,15 @@ export default defineComponent({
       products,
       category,
       chosenFile,
-      uploadAvatar
+      showButtonEdit,
+      uploadAvatar,
+      actionSaveProduct,
+      actionEditProduct,
+      actionInitDeleteProduct,
+      actionDeleteProduct,
+      actionInitEditProduct,
+      AddConfirmationModal,
+      deleteConfirmationModal,
     };
   },
 });
